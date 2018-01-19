afb7e9af9d2da57430a97239e0e9593fea46ad40
Satoshi Nishimura <nishim314@gmail.com>
2018-01-19 19:39:18 +0900

    change sql sample, index

--- db/tables.sql
+++ db/tables.sql
@@ -22,21 +22,3 @@ CREATE TABLE txindex(
   nextn int[] not null
 );
 CREATE INDEX txindex_txhash_btree on txindex (txhash);
-
-
---
--- SQL EXAMPLE
---
--- insert into txindex values('aaa'::bytea, 1, ARRAY[]::bytea[], ARRAY['addr1'::bytea], ARRAY['next1'::bytea]);
--- insert into txindex values('aaa'::bytea, 2, ARRAY[]::bytea[], ARRAY['addr2'::bytea], ARRAY['next1'::bytea]);
---
--- insert into txindex values('bbb'::bytea, 3, ARRAY[]::bytea[], ARRAY['addr1'::bytea], ARRAY[]::bytea[]);
-
--- select * from txindex where outaddr @> ARRAY['addr1'::bytea] AND nexthash[array_position(outaddr, 'addr1'::bytea)] is null order by height desc;
-
-
--- REAL SAMPLE
---
--- height: 1797 - 1798 address
--- select * from txindex where inaddr @> ARRAY[E'\\x6f4be8555a0aef70'::bytea] or outaddr @> ARRAY[E'\\x6f4be8555a0aef70'::bytea];
---
