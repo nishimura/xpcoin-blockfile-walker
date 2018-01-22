c29a33dab4abb84c3b37a4c4680cb89dbd3dfe0c
Satoshi Nishimura <nishim314@gmail.com>
2018-01-22 17:24:38 +0900

    change sql

--- db/indexes.sql
+++ db/indexes.sql
@@ -2,5 +2,5 @@ CREATE INDEX bindex_height_btree on bindex(height);
 
 
 
-create index txindex_outdata_addr_gin on txindex using gin(substrbytea(outdata, 1, 8));
-create index txindex_outdata_next_gin on txindex using gin(substrbytea(outdata, 1, 9));
+CREATE INDEX txindex_outdata_addr_gin on txindex using gin(substrbytea(outdata, 1, 8));
+CREATE INDEX txindex_outdata_next_gin on txindex using gin(substrbytea(outdata, 1, 9));
--- db/tables.sql
+++ db/tables.sql
@@ -29,6 +29,7 @@ CREATE INDEX txindex_txhash_btree on txindex (txhash);
 -- 18 - 25: next tx hash
 -- 26 - 29: next tx vin.n
 
+DROP FUNCTION IF EXISTS substrbytea(bytea[], int, int);
 CREATE FUNCTION substrbytea(bytea[], int, int) RETURNS bytea[] IMMUTABLE AS $$
   SELECT
     array_agg(substring(i from $2 for $3))
@@ -36,6 +37,7 @@ CREATE FUNCTION substrbytea(bytea[], int, int) RETURNS bytea[] IMMUTABLE AS $$
     unnest($1) AS t(i)
 $$ LANGUAGE SQL;
 
+DROP FUNCTION IF EXISTS hex_to_int(hexval bytea);
 CREATE FUNCTION hex_to_int(hexval bytea) RETURNS bigint IMMUTABLE AS $$
 DECLARE
     result bigint;
@@ -44,7 +46,7 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-
+DROP FUNCTION IF EXISTS bytea_to_amount(bytea[], bytea);
 CREATE FUNCTION bytea_to_amount(bytea[], bytea) RETURNS numeric IMMUTABLE AS $$
   SELECT
       sum(hex_to_int(substring(i from 10 for 8)))
