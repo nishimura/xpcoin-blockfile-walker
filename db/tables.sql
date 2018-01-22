3bb65a1928e330581916be4d76f162567ad5b347
Satoshi Nishimura <nishim314@gmail.com>
2018-01-22 16:21:06 +0900

    move sql

--- db/tables.sql
+++ db/tables.sql
@@ -20,5 +20,36 @@ CREATE TABLE txindex(
 );
 CREATE INDEX txindex_txhash_btree on txindex (txhash);
 
--- if txhash size is large:
---CREATE INDEX txindex_txhash_hash on txindex using hash (txhash);
+
+-- outdata
+--
+-- 1 - 8: nextaddr
+-- 9 - 9: unspent: 0x01, spent: 0x02
+-- 10 - 17: out value
+-- 18 - 25: next tx hash
+-- 26 - 29: next tx vin.n
+
+CREATE FUNCTION substrbytea(bytea[], int, int) RETURNS bytea[] IMMUTABLE AS $$
+  SELECT
+    array_agg(substring(i from $2 for $3))
+  FROM
+    unnest($1) AS t(i)
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION hex_to_int(hexval bytea) RETURNS bigint IMMUTABLE AS $$
+DECLARE
+    result bigint;
+BEGIN
+    EXECUTE 'SELECT x''' || encode(hexval, 'hex') || '''::bigint' INTO result; RETURN result;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION bytea_to_amount(bytea[], bytea) RETURNS numeric IMMUTABLE AS $$
+  SELECT
+      sum(hex_to_int(substring(i from 10 for 8)))
+  FROM
+    unnest($1) AS t(i)
+  WHERE
+    substring(i from 1 for 9) = $2 || E'\\x01'
+$$ LANGUAGE SQL;
